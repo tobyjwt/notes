af2ff7bcabc625ad4d35b9993270adb779a96941
jwentao <jin_wentao@qq.com>
2021-10-11 20:29:19 +0800

    refactor: 修改注释

--- js基础/demo/1.call&apply&bind.js
+++ js基础/demo/1.call&apply&bind.js
@@ -1,6 +1,6 @@
 Function.prototype.myCall = function (context, ...args) {
     const fn = Symbol('fn'); // 运用symbol的特性，防止覆盖context已有的属性
-    context = context || window; // 没有传入上下文的情况，将this指向window
+    context = context || window; // 在非严格模式下，this如果为null或者undefined会自动替换为全局对象，这个实现并不严谨，按浏览器环境下的非严格模式实现
     context[fn] = this; // this在这里就是函数实例本身，这里是利用函数内部的this指向函数调用者这一特性来改变函数内部this的指向
     const result = context[fn](...args); // 执行函数，缓存函数执行结果，因为在返回结果前要将context上的Symbol属性删除
     delete context[fn]; // 删除临时属性
