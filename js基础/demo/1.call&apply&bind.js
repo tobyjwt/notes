a916dd90974b1ea298f8b914db09cf72934ee75f
jwentao <jin_wentao@qq.com>
2021-10-11 20:24:56 +0800

    feat: call

--- js基础/demo/1.call&apply&bind.js
+++ js基础/demo/1.call&apply&bind.js
@@ -1,10 +1,10 @@
 Function.prototype.myCall = function (context, ...args) {
-    const fn = Symbol('fn');
-    context = context || window;
-    context[fn] = this;
-    const result = context[fn](...args);
-    delete context[fn];
-    return result;
+    const fn = Symbol('fn'); // 运用symbol的特性，防止覆盖context已有的属性
+    context = context || window; // 没有传入上下文的情况，将this指向window
+    context[fn] = this; // this在这里就是函数实例本身，这里是利用函数内部的this指向函数调用者这一特性来改变函数内部this的指向
+    const result = context[fn](...args); // 执行函数，缓存函数执行结果，因为在返回结果前要将context上的Symbol属性删除
+    delete context[fn]; // 删除临时属性
+    return result; // 返回结果
 }
 
 Function.prototype.myApply = function (context, args) {
